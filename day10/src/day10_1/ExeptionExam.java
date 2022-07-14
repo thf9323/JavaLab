267c6f03c29eeb4680644c184f1e09dd0aa41d46
thf9323 <80367291+thf9323@users.noreply.github.com>
2022-07-14 18:16:58 +0900

    7/14
    7/14

--- day10/src/day10_1/ExeptionExam.java
+++ day10/src/day10_1/ExeptionExam.java
@@ -4,7 +4,7 @@ public class ExeptionExam {
 	public int get50thItem(int []array) throws IllegalArgumentException{
 		if(array.length<50) {
 			throw new IllegalArgumentException();
-		}
+		}  
 		return array[49];
 	}
 }
